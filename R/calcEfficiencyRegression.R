1a045b402e23562b5b389fcf6eeb3112fe5d290d
hagento <hagento@pik-potsdam.de>
2025-02-11 15:07:22 +0100

    refactored calcEfficiencyRegression

--- R/calcEfficiencyRegression.R
+++ R/calcEfficiencyRegression.R
@@ -9,6 +9,8 @@
 #' This approach closely follows the model by De Stercke et al. which is
 #' mainly driven by GDP per Capita.
 #'
+#' @param gasBioEquality Determines if natural gas and modern biomass share the same efficiencies
+#'
 #' @references De Stercke, S. (2014). Dynamics of Energy Systems: A Useful
 #' Perspective (Interim Report, p. 68). IIASA.
 #' http://pure.iiasa.ac.at/id/eprint/11254/1/IR-14-013.pdf
@@ -17,7 +19,7 @@
 #'
 #' @importFrom quitte aggregate_map removeColNa as.quitte
 #' @importFrom stats as.formula na.omit nls
-#' @importFrom dplyr mutate select filter left_join group_by across all_of rename
+#' @importFrom dplyr mutate select filter left_join group_by across all_of rename coalesce ends_with %>% .data
 #' @importFrom tidyr spread unite
 #' @importFrom madrat calcOutput toolGetMapping
 #' @importFrom magclass as.magpie
@@ -25,7 +27,7 @@
 #' @export
 
 
-calcEfficiencyRegression <- function() {
+calcEfficiencyRegression <- function(gasBioEquality = TRUE) {
 
   # FUNCTIONS ------------------------------------------------------------------
 
@@ -72,10 +74,9 @@ calcEfficiencyRegression <- function() {
   pop <- calcOutput("PopulationPast", aggregate = FALSE) %>%
     as.quitte()
 
-  # regression parameter corrections
-  parsCorr <- toolGetMapping(name  = "correct_efficiencies.csv",
-                             type  = "sectoral",
-                             where = "mredgebuildings")
+  # Regression parameter corrections
+  parsCorrections <- toolGetMapping("correctEfficiencies.csv", type = "sectoral")
+
 
 
   # PARAMETERS -----------------------------------------------------------------
@@ -83,13 +84,19 @@ calcEfficiencyRegression <- function() {
   # Minimum Requirement to be considered
   minEfficiency <- 0.05
 
+  # Equal efficiencies
+  equalEfficiencies <- toolGetMapping("equalEfficiencies.csv",
+                                      type = "sectoral",
+                                      where = "mredgebuildings")
+
+
 
   # PROCESS DATA ---------------------------------------------------------------
 
   #--- Calculate existing FE-EU efficiencies
 
   # Aggregate PFU Data to PFU Country Code
-  data <- pfu %>%
+  pfuAggregate <- pfu %>%
     mutate(value = replace_na(.data[["value"]], 0)) %>%
     unite("variable", "enduse", "carrier", sep = ".") %>%
     aggregate_map(mapping = regionmapping[!is.na(regionmapping$PFUDB), c("iso", "PFUDB")],
@@ -97,7 +104,7 @@ calcEfficiencyRegression <- function() {
                   forceAggregation = TRUE)
 
   # Aggregate GDPpop to PFU Country Code
-  gdppop <- gdppop %>%
+  gdppopAggregate <- gdppop %>%
     aggregate_map(mapping = regionmapping[!is.na(regionmapping$PFUDB), c("iso", "PFUDB")],
                   by = c("region" = "iso"),
                   forceAggregation = TRUE,
@@ -110,71 +117,69 @@ calcEfficiencyRegression <- function() {
 
 
   # Combine with GDP per Cap
-  data <- data %>%
-    left_join(gdppop %>%
+  data <- pfuAggregate %>%
+    left_join(gdppopAggregate %>%
                 rename(gdppop = "value"),
               by = c("region", "period"))
 
-  # Calculate Efficiencies as Regression-Input
-  data <- data %>%
+
+  histEfficiencies <- data %>%
+    # Calculate efficiencies from UE/FE data
     select(-"model", -"scenario") %>%
     spread(.data[["unit"]], .data[["value"]]) %>%
     mutate(value = .data[["ue"]] / .data[["fe"]]) %>%
-    select(-"fe", -"ue")
+    select(-"fe", -"ue") %>%
 
-  # Filter out unrealistic Efficiencies
-  data <- filter(data, .data[["value"]] > minEfficiency)
-
-  vars <- data %>%
-    group_by(across(all_of("variable"))) %>%
-    filter(.data[["variable"]] != "gdppop",
-           !all(is.na(.data[["value"]]))) %>%
-    getElement("variable") %>%
-    unique()
+    # Filter out unrealistic efficiencies
+    filter(.data[["value"]] > minEfficiency)
 
 
-  #--- Calculate Parameters
-  parsFull <- data.frame()
+  euecCombinations <- unique(histEfficiencies$variable)
 
-  for (var in vars) {
-    pars <- calcPars(data, var)
-    parsFull <- as.data.frame(do.call(cbind, as.list(pars))) %>%
-      mutate(variable = var) %>%
-      rbind(parsFull)
+
+  #--- Calculate regression parameter
+  fitPars <- data.frame()
+
+  for (euec in euecCombinations) {
+    pars <- calcPars(histEfficiencies, euec)
+    fitPars <- as.data.frame(do.call(cbind, as.list(pars))) %>%
+      mutate(variable = euec) %>%
+      rbind(fitPars)
   }
-
-  parsFull <- parsFull %>%
-    separate("variable", c("enduse", "carrier"), sep = "\\.")
 
 
 
   # CORRECTIONS ----------------------------------------------------------------
 
-  # Rename corrected parameters
-  parsCorr <- parsCorr %>%
-    rename(AsymCorr = "Asym", lrcCorr = "lrc", R0Corr = "R0")
+  # Replace regression parameters to account for RH/HP mix in electric heating end-uses
+  fitPars <- fitPars %>%
+    left_join(parsCorrections, by = "variable", suffix = c("", ".corr")) %>%
+    mutate(Asym = coalesce(.data[["Asym.corr"]], .data[["Asym"]]),
+           R0   = coalesce(.data[["R0.corr"]],   .data[["R0"]]),
+           lrc  = coalesce(.data[["lrc.corr"]],  .data[["lrc"]])) %>%
+    select(-ends_with(".corr"))
 
 
-  # Correct Regression Parameters for electrical Heat Transfer Technologies
-  parsFull <- parsFull %>%
-    left_join(parsCorr, by = c("carrier", "enduse")) %>%
-    mutate(Asym = ifelse(is.na(.data[["AsymCorr"]]), .data[["Asym"]], .data[["AsymCorr"]]),
-           R0 = ifelse(is.na(.data[["R0Corr"]]), .data[["R0"]], .data[["R0Corr"]]),
-           lrc = ifelse(is.na(.data[["lrcCorr"]]), .data[["lrc"]], .data[["lrcCorr"]])) %>%
-    select(-"AsymCorr", -"R0Corr", -"lrcCorr")
+  # Correct efficiencies of services assumed to be equal
+  fitPars <- fitPars %>%
+    left_join(equalEfficiencies, by = "variable") %>%
+    mutate(across(c("Asym", "R0", "lrc"),
+                  ~coalesce(.x[match("equalTo", "variable")], .x))) %>%
+    select(-"equalTo")
 
 
 
   # OUTPUT ---------------------------------------------------------------------
 
   # Trim Dataframe
-  parsFull <- parsFull %>%
+  fitPars <- fitPars %>%
+    separate("variable", c("enduse", "carrier"), sep = "\\.") %>%
     mutate(region = "GLO") %>%
     select("region", "carrier", "enduse", "Asym", "R0", "lrc") %>%
     as.magpie()
 
 
-  return(list(x = parsFull,
+  return(list(x = fitPars,
               isocountries = FALSE,
               description = "Regression Parameter for FE-UE-Efficiency Projection",
               unit = ""))
